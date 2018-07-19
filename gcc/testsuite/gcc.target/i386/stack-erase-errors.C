7666b82e676e1efa4f9a67fa11e14050588acf3d
Craig Blackmore <craig.blackmore@embecosm.com>
2018-07-19 11:44:05 +0100

    Add C++ stack-erase tests

---
+++ gcc/testsuite/gcc.target/i386/stack-erase-errors.C
@@ -0,0 +1,136 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fno-stack-erase" } */
+
+/* Non-stack erase function does not result in a clear stack.  */
+int f1_sub (int x, int y)
+{
+  int a[300], s;
+  for (int i = 0; i < x; i++)
+  {
+    a[i] = a[i] + x;
+  }
+
+  return a[y];
+}
+
+/* Stack erase function calling non-stack erase function */
+/* should we generate a warning/error for this scenario? */
+__attribute__((stack_erase))
+int f1 (int x, int y)
+{
+  int a = f1_sub (x, y); /* { dg-error "Cannot call non-stack-erase function 'int f1_sub.int, int.' from stack-erase function" } */
+  return a + 4;
+}
+
+// __attribute__((stack_erase))
+// int f2_sub (int (*myfunc) (int, int) __attribute__((stack_erase)), int x, int y)
+// {
+//   myfunc(x, y); // should be an error here
+// }
+
+// __attribute__((stack_erase))
+// int f2 (int x, int y)
+// {
+//   f2_sub (f1_sub, x ,y);
+// }
+
+/* calling by pointer-to-function not allowed in stack-erase function */
+__attribute__((stack_erase))
+int f2_sub (int (*myfunc) (int, int), int x, int y)
+{
+  int a = myfunc(x, y); /* { dg-error "cannot call using function pointer .myfunc. from stack.erase function" } */
+  return a + 5;
+}
+
+__attribute__((stack_erase))
+int f2 (int x, int y)
+{
+  int a = f2_sub (f1_sub, x ,y);
+  return a + 6;
+}
+
+__attribute__((stack_erase))
+int f3 (int x, int y); /* { dg-error "'stack_erase' attribute present on 'int f3.int, int.'" } */
+
+int f3 (int x, int y) /* { dg-error "but not here" } */
+{
+  return x + y;
+}
+
+int f4 (int x, int y); /* { dg-error "but not here" } */
+
+__attribute__((stack_erase))
+int f4 (int x, int y) /* { dg-error "'stack_erase' attribute present on 'int f4.int, int.'" } */
+{
+  return x + y;
+}
+
+/* always inline stack erase function error */
+// __attribute__((stack_erase, always_inline))
+// int f5 (int x, int y) {
+//   return x + y;
+// }
+
+int test(int (*func) (int, int))
+{
+  // Zero 2kb of stack space and record SP
+  void *initial_sp;
+  void *t1;
+  __asm__ volatile (
+    "  movq    %%rsp, %1\n"
+    "  subq    $2048, %1\n"
+    "1:\n"
+    "  cmpq    %%rsp, %1\n"
+    "  je      2f\n"
+    "  pushq   $0\n"
+    "  jmp     1b\n"
+    "2:\n"
+    "  addq    $2048, %%rsp\n" // restore sp to initial value
+    "  movq    %%rsp, %0" // restore sp to initial value
+    : "=r" (initial_sp), "+r" (t1) : : );
+
+  // Call test function
+  func(250, 240);
+
+  // Check stack pointer is unchanged after return and that stack is zeroed.
+  int stack_check;
+  void *t2;
+  __asm__ volatile (
+  "  cmpq    %%rsp, %3\n" // check rsp unchanged
+  "  jne     3f\n"
+  "  movq    %%rsp, %1\n"
+  "  subq    $2048, %%rsp\n" // goto to top of region to check
+  "  subq    $8, %1\n" // ignore callee return address
+  "1:\n"
+  "  cmpq    %1, %%rsp\n"
+  "  je      4f\n"
+  "  popq    %2\n"
+  "  cmpq    $0, %2\n"
+  "  jne     2f\n"
+  "  jmp 1b\n"
+  "2:\n"
+  "  movl    $1, %0\n"
+  "  jmp     5f\n"
+  "3:\n"
+  "  movl    $2, %0\n"
+  "  jmp     5f\n"
+  "4:\n"
+  "  movl    $0, %0\n"
+  "  jmp     5f\n"
+  "5:\n"
+  "  movq    %3, %%rsp"
+  : "+r" (stack_check), "+r" (t1), "+r" (t2)
+  : "r" (initial_sp) : );
+
+  return stack_check;
+}
+
+int main (void) {
+  int r;
+  /* Stack erase function calling non-stack erase function. */
+  if ((r = test (f1)))
+    return 1;
+  if ((r = test (f3)))
+    return 3;
+  return 0;
+}
---
+++ gcc/testsuite/gcc.target/i386/stack-erase.C
@@ -0,0 +1,219 @@
+/* { dg-do run } */
+/* { dg-options "-O0 -fno-stack-erase" } */
+
+/* Simple function that uses the stack */
+__attribute__((stack_erase))
+int f1 (int x, int y)
+{
+  int a[300], s;
+  for (int i = 0; i < x; i++)
+  {
+    a[i] = a[i] + x;
+  }
+  return a[y];
+}
+
+/* For preventing VLA in f2 getting optimized away.  */
+int f2_glob = 7;
+
+__attribute__((noinline, stack_erase))
+int f2_sub ()
+{
+  return f2_glob;
+}
+
+/* Function using VLA */
+__attribute__((stack_erase))
+int f2 (int x, int y)
+{
+  int l = x + 2;
+  int a[l];
+  int s = 0;
+
+  for (int i = 0; i < l; i++)
+  {
+    // The caller is responsible for removing the return address used by f2_sub
+    a[i] = f2_sub ();
+  }
+
+
+  return a[y];
+}
+
+/* Stack erase function calling another stack erase function.  */
+__attribute__((stack_erase))
+int f3 (int x, int y)
+{
+  int a = f1 (x, y);
+  return a + 2;
+}
+
+
+/* Non-stack erase function calling a stack erase function does not result in
+   a clear stack.  */
+int f4 (int x, int y)
+{
+  int a = f1 (x, y);
+  return a + 3;
+}
+
+/* Non-stack erase function does not result in a clear stack.  */
+int f5 (int x, int y)
+{
+  int a[300], s;
+  for (int i = 0; i < x; i++)
+  {
+    a[i] = a[i] + x;
+  }
+
+  return a[y];
+}
+
+/* Function using VLA inside a scope.  */
+__attribute__((stack_erase))
+int f6 (int x, int y)
+{
+  int l = x + 2;
+  int a[l];
+
+  for (int i = 0; i < l; i++)
+  {
+    int b[i];
+    int s = 0;
+    for (int j = 0; j < i; j++)
+      s += f2_sub();
+    a[i] = s;
+  }
+
+  return a[y];
+}
+
+/* tail call */
+/* f7 pushes 200 more ints onto the stack than f1 */
+__attribute__((stack_erase))
+int f7 (int x, int y) {
+  volatile int a[500], s;
+  for (int i = 0; i < x; i++)
+  {
+    a[i] = a[i] + x;
+  }
+  return f1(x, y);
+}
+
+/* recursion */
+__attribute__((stack_erase))
+int fib(int i)
+{
+  volatile int a = 5678;
+  if(i==0)
+    return 1;
+  if(i==1)
+    return 1;
+  return fib(i-1) + fib(i-2);
+}
+
+__attribute__((stack_erase))
+int f8 (int x, int y) {
+  volatile int a = 1234;
+  return fib(x-y);
+}
+
+__attribute__((stack_erase))
+int f9_args(int a0,  int a1,  int a2,  int a3,  int a4,  int a5,  int a6,  int a7,
+             int a8,  int a9,  int a10, int a11, int a12, int a13, int a14, int a15,
+             int a16, int a17, int a18, int a19, int a20, int a21, int a22, int a23,
+             int a24, int a25) {
+  int a = f1 (a0, a1);
+  return a + 7;  
+}
+
+/* test that caller clears pushed arguments */
+__attribute__((stack_erase))
+int f9 (int x, int y) {
+  int a = f1 (x, y);
+  int b = f9_args(0,  1,  2,  3,  4,  5,  6,  7, 
+                   8,  9,  10, 11, 12, 13, 14, 15,
+                   16, 17, 18, 19, 20, 21, 22, 23,
+                   24, 25);
+  return a + b + 10;
+}
+
+int test(int (*func) (int, int))
+{
+  // Zero 2kb of stack space and record SP
+  void *initial_sp;
+  void *t1;
+  __asm__ volatile (
+    "  movq    %%rsp, %1\n"
+    "  subq    $2048, %1\n"
+    "1:\n"
+    "  cmpq    %%rsp, %1\n"
+    "  je      2f\n"
+    "  pushq   $0\n"
+    "  jmp     1b\n"
+    "2:\n"
+    "  addq    $2048, %%rsp\n" // restore sp to initial value
+    "  movq    %%rsp, %0" // restore sp to initial value
+    : "=r" (initial_sp), "+r" (t1) : : );
+
+  // Call test function
+  func(250, 240);
+
+  // Check stack pointer is unchanged after return and that stack is zeroed.
+  int stack_check;
+  void *t2;
+  __asm__ volatile (
+  "  cmpq    %%rsp, %3\n" // check rsp unchanged
+  "  jne     3f\n"
+  "  movq    %%rsp, %1\n"
+  "  subq    $2048, %%rsp\n" // goto to top of region to check
+  "  subq    $8, %1\n" // ignore callee return address
+  "1:\n"
+  "  cmpq    %1, %%rsp\n"
+  "  je      4f\n"
+  "  popq    %2\n"
+  "  cmpq    $0, %2\n"
+  "  jne     2f\n"
+  "  jmp 1b\n"
+  "2:\n"
+  "  movl    $1, %0\n"
+  "  jmp     5f\n"
+  "3:\n"
+  "  movl    $2, %0\n"
+  "  jmp     6f\n" // if sp changed, retain new value to return properly
+  "4:\n"
+  "  movl    $0, %0\n"
+  "  jmp     5f\n"
+  "5:\n"
+  "  movq    %3, %%rsp\n"
+  "6:\n"
+  : "+r" (stack_check), "+r" (t1), "+r" (t2)
+  : "r" (initial_sp) : );
+
+  return stack_check;
+}
+
+int main (void) {
+  int r;
+  if ((r = test (f1)))
+    return 1;
+  if ((r = test (f2)))
+    return 2;
+  if ((r = test (f3)))
+    return 3;
+  /* Non-stack erase function calling stack erase function.  */
+  if ((r = test (f4)) != 1)
+    return 4;
+  /* Non-stack erase function.  */
+  if ((r = test (f5)) != 1)
+    return 5;
+  if ((r = test (f6)))
+    return 6;
+  if ((r = test (f7)))
+    return 7;
+  if ((r = test (f8)))
+    return 8;
+  if ((r = test (f9)))
+    return 9;
+  return 0;
+}
